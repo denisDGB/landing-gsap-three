13af79a852b91b8ba044199678a321bd0f6803f2
Denis Gómez <denis.gomez171997@gmail.com>
2025-03-22 00:23:16 -0600

    ✅ Corrige API_URL config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,8 @@
 // config.js
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+"use client";
+
+const isDev = process.env.NODE_ENV === "development";
+
+export const API_URL = isDev
+  ? "http://localhost:5000"
+  : process.env.NEXT_PUBLIC_API_URL;
