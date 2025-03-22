8a86b31dd78097623b14569a28124920d99f67cb
Denis Gómez <denis.gomez171997@gmail.com>
2025-03-22 05:58:40 -0600

    ✅ Corrige API_URL config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
 // config.js
 "use client";
 
-const isDev = process.env.NODE_ENV === "development";
+console.log("🌐 NODE_ENV:", process.env.NODE_ENV);
+console.log("🔗 NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
 
-export const API_URL = isDev
-  ? "http://localhost:5000"
-  : process.env.NEXT_PUBLIC_API_URL;
+const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : process.env.NEXT_PUBLIC_API_URL || "https://landing-gsap-three-production-3b59.up.railway.app";
+
+export { API_URL };
